2995e79fe22cdd4c749bce78444c264aa1a19182
Lubomir <lubomir_stoykov@abv.bg>
2022-04-22 09:29:18 -0400

    Added check for name and extension

--- unpack.sh
+++ unpack.sh
@@ -15,9 +15,25 @@ unzip -q "${zip}" -d "${tmp}"
 
 for d in $(find $tmp -mindepth 1 -maxdepth 1)
 do
-	fn=$(echo $d | cut -d '/' -f 4 | cut -d '-' -f 1)
+	fn="$(echo $d | cut -d '/' -f 4 | cut -d '-' -f 1)"
+	line=$fn
+	archive="$(find $d -mindepth 1 -maxdepth 1 | cut -d '/' -f 5)"
 	
-	echo $fn
+	#check name and extension
+
+	if [ "$fn.tar.xz" = "$archive" ]
+	then
+		line="$line 0"
+	else
+		line="$line 1"
+	fi
+
+	
+
+
+		
+	#echo $line
+	#echo "{line}" >> $result
 done
 
 
